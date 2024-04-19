12ab31a0da67ae4583079e6bd484d56d009cdbca
Ajith Ajayan <ajithajayan222aa@gmail.com>
2024-04-19 17:05:26 +0530

    first commit

---
+++ ecommerce/src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import '../../assets/Styles/Loader.scss'
+
+function Loader() {
+  return (
+    <>
+      <div class="loader">
+        Loading
+        <span></span>
+      </div>
+    </>
+  );
+}
+
+export default Loader;
